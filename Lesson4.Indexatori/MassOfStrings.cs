44b23398035d78dc155d1e187bf73aa50149e22a
a a <doejayne20@gmail.com>
2024-09-13 16:49:44 +0200

    HW

---
+++ Lesson4.Indexatori/MassOfStrings.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson4.Indexatori
+{
+
+    internal class MassOfStrings
+    {
+        private List<string> stringList;
+        private int count;
+
+        public MassOfStrings(int count)
+        {
+            this.count = count;
+            this.stringList = new List<string>(count);
+            //for (int i = 0; i < count; i++)
+            //{
+            //    Console.Write("Введите строку: ");
+            //    stringList.Add(Console.ReadLine()!);
+            //}
+        }
+        public string this[int index]
+        {
+            get {if (index >= 0 && index < count) return stringList[index];
+                else return null;
+            }
+            set { stringList[index] = value; }
+        }
+        public void Add(string value)
+        {
+            stringList.Add(value);
+        }
+        public void AddList(List<string> list)
+        {
+            stringList.AddRange(list);
+        }
+
+    }
+}
---
+++ Lesson4.Indexatori/Program.cs
@@ -0,0 +1,15 @@
+﻿// Индексаторы
+
+using Lesson4.Indexatori;
+List<string> list = new List<string>()
+{
+    "Cat",
+    "Dog",
+    "Bird",
+    "Mouse",
+    "Goose",
+    "Beaver"
+};
+MassOfStrings stringList = new MassOfStrings(6);
+stringList.
+
